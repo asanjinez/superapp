7ff6c6591e065ccf8cee1a7a5189f2d000227308
asanjinez <asanjinez@hexacta.com>
2023-08-11 11:49:39 -0300

    project init

---
+++ src/main/java/com/superapp/SuperappApplication.java
@@ -0,0 +1,14 @@
+package com.superapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperappApplication.class, args);
+		System.out.println("hola superappers");
+	}
+
+}
