4dd6fcfc0afe651f2234dce04689d06de6f99f6e
asanjinez <asanjinez@hexacta.com>
2023-08-15 10:47:42 -0300

    Product class created

---
+++ src/main/java/com/superapp/product/Product.java
@@ -0,0 +1,23 @@
+package com.superapp.product;
+
+import com.superapp.persona.EType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@Table(name = "product")
+@AllArgsConstructor @NoArgsConstructor
+@Getter @Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "unitPrice")
+    private Integer unitPrice;
+}
