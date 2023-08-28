5e04f72babdad5a4b3ac4d421a1dc5dc0fa8568b
asanjinez <asanjinez@hexacta.com>
2023-08-28 12:18:14 -0300

    better product service + multiple db working

--- product-service/src/main/java/com/superapp/product_service/exception/NoProductFoundException.java
+++ product-service/src/main/java/com/superapp/product_service/exception/NoProductFoundException.java
@@ -1,4 +1,4 @@
-package com.superapp.exception;
+package com.superapp.product_service.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
